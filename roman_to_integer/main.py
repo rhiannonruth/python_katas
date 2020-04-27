178e784e4230f4c47b3e0a50919c1566535fe28f
rhiannonruth <rhiannonruth@hotmail.co.uk>
2020-04-27 17:07:59 +0100

    refactor to remove redundant if

--- roman_to_integer/main.py
+++ roman_to_integer/main.py
@@ -4,8 +4,6 @@ VALUE_MAP = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 def roman_to_integer(roman):
     if len(roman) == 0:
         return 0
-    elif len(roman) == 1:
-        return VALUE_MAP[roman[0]]
     elif len(roman) >= 2 and VALUE_MAP[roman[0]] < VALUE_MAP[roman[1]]:
         return VALUE_MAP[roman[1]] - VALUE_MAP[roman[0]] + roman_to_integer(roman[2:])
     else:
